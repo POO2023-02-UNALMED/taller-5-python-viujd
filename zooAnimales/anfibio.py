4a3e786e80482174d02ff152f7659a3d29b2e14b
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-09-25 22:33:13 -0500

    Update anfibio.py

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -42,7 +42,7 @@ class Anfibio(Animal):
     def setColorEscamas(self, colorPiel):
         self._colorPiel=colorPiel
 
-    def getVenenoso(self):
+    def isVenenoso(self):
         return self._venenoso
     
     def setVenenoso(self, venenoso):
